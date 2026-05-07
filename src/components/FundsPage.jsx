bed121e513138aa9375e58f73e04154aae71cf28
harshit9n <pharshit515@gmail.com>
2026-05-07 13:33:45 +0530

    Fix wallet balance not updating

--- src/components/FundsPage.jsx
+++ src/components/FundsPage.jsx
@@ -21,7 +21,7 @@ import Sidebar from './Sidebar';
 import AuthPopup from './AuthPopup';
 import { db, auth } from "../firebase";
 import { onAuthStateChanged, reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
-import { collection, addDoc, onSnapshot, doc, query, where, serverTimestamp, getDocs, orderBy, limit, updateDoc, increment } from "firebase/firestore";
+import { collection, addDoc, onSnapshot, doc, query, where, serverTimestamp, getDocs, orderBy, limit, updateDoc, increment, runTransaction } from "firebase/firestore";
 import banner1 from '../assets/banner1.png';
 import banner2 from '../assets/banner2.png';
 import banner3 from '../assets/banner3.png';
